95f346bf0405d966cb9fdbd6284cb41a342a0cff
Appurupai <56392268+Appurupai3@users.noreply.github.com>
2026-01-20 17:13:50 +0800

    Remove quick 60-70-80-90 button

--- Frontend/src/components/OrderForm.vue
+++ Frontend/src/components/OrderForm.vue
@@ -53,14 +53,6 @@
             套用代碼
           </button>
         </div>
-        <div class="flex flex-wrap gap-2">
-          <button
-            @click="applyCode('60-70-80-90')"
-            class="px-4 py-2 bg-indigo-100 text-indigo-700 rounded-lg hover:bg-indigo-200 transition-all duration-300 font-medium"
-          >
-            快速 60-70-80-90
-          </button>
-        </div>
       </div>
     </div>
 
